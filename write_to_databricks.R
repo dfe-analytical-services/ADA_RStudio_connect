3981db40eb5df55793676bf24f25b3ef08ecd33c
FOSTER <Charlotte.FOSTER@education.gov.uk>
2023-12-07 11:40:02 +0000

    add write_to_databricks guidance

---
+++ write_to_databricks.R
@@ -0,0 +1,27 @@
+# NOTE this code is a template, you must replace any code that starts with 
+# 'INSERT_' with a valid name in order to run.
+
+library(DBI)
+
+# In order to perform these actions, the user must have CREATE SCHEMA and CREATE TABLE permissions 
+# for the current database.
+
+# Define what you want the schema and name to be in Databricks of the table you are writing
+table_spec <- DBI::Id(schema = "INSERT_SCHEMA", table = "INSERT_DESIRED_NAME")
+
+# This creates an empty table with the right variable names
+DBI::dbCreateTable(
+  con,
+  name = table_spec,
+  
+  # Column names/types will be inferred from the structure of the table
+  fields = INSERT_TABLE_OBJECT # a table you have created in R Sstudio that you now wish to save to Databricks
+)
+
+# This adds the data into the table that was made in the last step
+DBI::dbAppendTable(
+  con,
+  name = table_spec,
+  value = INSERT_TABLE_OBJECT # a table you have created in R Sstudio that you now wish to save to Databricks
+)
+
